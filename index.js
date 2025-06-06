6aa69771114d1cdd427eb09867a321802bbc9de2
shivhare08 <shivhares2002@gmail.com>
2025-06-06 22:17:10 +0530

    1st commit

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ const contactRouter = require('./controllers/contactController');
 
 //mongoose.connect('mongodb+srv://shivhares2002:mww8frbY4dnHF92a@cluster0.gq0hu.mongodb.net/basicBackend');
 //export const live_url = "mongodb+srv://shivhares2002:FIZDM8qUwguT9Zhh@courseera-app.d8d6i.mongodb.net/?retryWrites=true&w=majority&appName=courseera-app"
-function connectdb(){
-    mongoose.connect('mongodb+srv://shivhares2002:vA5koFSpNO1GstMb@cluster0.idnifnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    )
-}
+// function connectdb(){
+//     mongoose.connect('mongodb+srv://shivhares2002:vA5koFSpNO1GstMb@cluster0.idnifnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+//     )
+// }
 
-connectdb();
+// connectdb();
+
+mongoose.connect('mongodb+srv://shivhares2002:J1uoYmwpmQrirlkg@careerapp.6dkf0hq.mongodb.net/?retryWrites=true&w=majority&appName=careerApp')
+
 app.use(express.urlencoded({extended:false}));
 app.use(expressFileUpload({
     useTempFiles:true
